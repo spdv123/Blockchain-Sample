cd6e585a4a7e727b323f08268e7bfa011cf2cdfa
aaron <3303085681@qq.com>
2016-11-12 20:44:28 +0800

    edit miner.py

---
+++ blockchain/miner.py
@@ -0,0 +1,21 @@
+import hashlib
+
+class BitcoinMiner(object):
+    """
+    bitcoin miner
+    """
+
+    def miner(_self, data, target):
+        nonce = 0;
+
+        while True:
+            hexDig = hashlib.sha256((data + str(nonce)).encode()).hexdigest()                        
+            if hexDig.startswith(target):
+                return hexDig, nonce                
+            nonce += 1
+             
+if __name__ == '__main__': 
+    client = BitcoinMiner()
+    hashedValue, nonce = client.miner('This is a string', '0000')
+    print(hashedValue)
+    print(nonce)
